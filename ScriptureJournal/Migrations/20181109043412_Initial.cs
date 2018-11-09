44fd6cb211cd58466b7f8147f7a6d355bdc57024
David Stoddard <david@davidstorytime.com>
2018-11-08 22:32:32 -0700

    Set up database, adjust CSS, and more than I should have for one push

---
+++ ScriptureJournal/Migrations/20181109043412_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScriptureJournal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scripture",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Book = table.Column<string>(nullable: true),
+                    Chapter = table.Column<int>(nullable: false),
+                    Verse = table.Column<int>(nullable: false),
+                    Note = table.Column<string>(nullable: true),
+                    AddedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scripture", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scripture");
+        }
+    }
+}
---
+++ ScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptureJournal.Models
+{
+  public class Scripture
+  {
+    public int ID { get; set; }
+    public string Book { get; set; }
+    public int Chapter { get; set; }
+    public int Verse { get; set; }
+    public string Note { get; set; }
+    public DateTime AddedDate { get; set; }
+  }
+}
