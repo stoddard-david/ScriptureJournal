9424cbb23584d81218639fb41bb3391a2587acf0
David Stoddard <david@davidstorytime.com>
2018-11-10 00:35:34 -0700

    Another added more than I should (Styling, sorting, searching)

--- ScriptureJournal/Models/Scripture.cs
+++ ScriptureJournal/Models/Scripture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,24 @@ namespace ScriptureJournal.Models
   public class Scripture
   {
     public int ID { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string Book { get; set; }
+
+    [Range(1, 200)]
+    [Required]
     public int Chapter { get; set; }
+
+    [Range(1, 200)]
+    [Required]
     public int Verse { get; set; }
+
+
     public string Note { get; set; }
+
+    [Display(Name = "Added Date")]
+    [DataType(DataType.Date)]
     public DateTime AddedDate { get; set; }
   }
 }
--- ScriptureJournal/Pages/Scriptures/Index.cshtml.cs
+++ ScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ScriptureJournal.Models;
 
@@ -19,10 +20,47 @@ namespace ScriptureJournal.Pages.Scriptures
         }
 
         public IList<Scripture> Scripture { get;set; }
+        public string SearchString { get; set; }
+        public SelectList Books { get; set; }
+        public string SearchBook { get; set; }
+        public string SortBy { get; set; }
 
-        public async Task OnGetAsync()
+
+    public async Task OnGetAsync(string searchString, string searchBook, string sortBy)
+      {
+      // Use LINQ to get list of genres.
+      IQueryable<string> bookQuery = from m in _context.Scripture
+                                      orderby m.Book
+                                      select m.Book;
+
+      var scriptures = from m in _context.Scripture
+                    select m;
+
+      if (!String.IsNullOrEmpty(sortBy))
+      {
+        if (sortBy == "Book") { 
+          scriptures = scriptures.OrderBy(o => o.Book);
+        } else 
         {
-            Scripture = await _context.Scripture.ToListAsync();
+          scriptures = scriptures.OrderBy(o => o.AddedDate);
         }
+      }
+
+      if (!String.IsNullOrEmpty(searchString))
+      {
+        scriptures = scriptures.Where(s => s.Note.Contains(searchString));
+      }
+
+      if (!String.IsNullOrEmpty(searchBook))
+      {
+        scriptures = scriptures.Where(x => x.Book == searchBook);
+      }
+
+      Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+      Scripture = await scriptures.ToListAsync();
+      SearchString = searchString;
+      SearchBook = searchBook;
+      SortBy = sortBy;
+      }
     }
 }
